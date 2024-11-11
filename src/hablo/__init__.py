ea61921fe786a48fe60be6839cfed699a2255296
Marco Weber <marcoweber@microsoft.com>
2024-11-11 15:50:25 +0100

    added some docs

--- src/hablo/__init__.py
+++ src/hablo/__init__.py
@@ -8,6 +8,11 @@ def mucho(
     con : Channel = GunicornChannel(),
     configuration : Union[RootConfiguration, None] = None
 ):
+    """
+    Main Unified Chat-Host Orchestration (MUCHO) function
+    This is the main entry point for the hablo library
+    usage: hablo.mucho(con=Channel)
+    """
     if configuration is not None:
         configuration = FileConfiguration(configpath="hablo.yaml")
     con.setConfiguration(configuration)
